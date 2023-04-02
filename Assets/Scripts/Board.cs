6f9c2fa1657ae5b92b4d8a29ef5d514108bfdbff
mbattaloglu <mmbattaloglu@gmail.com>
2023-04-02 14:01:54 +0300

    Word Guessing and Typing Handling

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,142 @@
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    private static readonly KeyCode[] SUPPORTED_KEYS = new KeyCode[]
+    {
+        KeyCode.A, KeyCode.B, KeyCode.C,
+        KeyCode.D, KeyCode.E, KeyCode.F,
+        KeyCode.G, KeyCode.H, KeyCode.I,
+        KeyCode.J, KeyCode.K, KeyCode.L,
+        KeyCode.M, KeyCode.N, KeyCode.O,
+        KeyCode.P, KeyCode.Q, KeyCode.R,
+        KeyCode.S, KeyCode.T, KeyCode.U,
+        KeyCode.V, KeyCode.W, KeyCode.X,
+        KeyCode.Y, KeyCode.Z
+    };
+
+    public Row[] rows { get; private set; }
+
+    private string[] solutions;
+    private string[] validWords;
+
+    private string word;
+
+    private int rowIndex;
+    private int columnIndex;
+
+    [Header("States")]
+    public Tile.State emptyState;
+    public Tile.State occupiedState;
+    public Tile.State correctState;
+    public Tile.State wrongSpotState;
+    public Tile.State incorrectState;
+
+
+    private void Awake()
+    {
+        rows = GetComponentsInChildren<Row>();
+    }
+
+    private void Start()
+    {
+        LoadData();
+        SetRandomWord();
+    }
+
+    private void Update()
+    {
+        Row currentRow = rows[rowIndex];
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            columnIndex = Mathf.Max(columnIndex - 1, 0);
+            currentRow.tiles[columnIndex].SetLetter('\0'); // null character
+            currentRow.tiles[columnIndex].SetState(emptyState);
+        }
+        else if (columnIndex >= currentRow.tiles.Length)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                SubmitRow(currentRow);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < SUPPORTED_KEYS.Length; i++)
+            {
+                if (Input.GetKeyDown(SUPPORTED_KEYS[i]))
+                {
+                    currentRow.tiles[columnIndex].SetLetter((char)SUPPORTED_KEYS[i]);
+                    currentRow.tiles[columnIndex].SetState(occupiedState);
+                    columnIndex++;
+                    break;
+                }
+            }
+        }
+    }
+
+    private void LoadData()
+    {
+        TextAsset textFile = Resources.Load("official_wordle_all") as TextAsset;
+        validWords = textFile.text.Split('\n');
+
+        textFile = Resources.Load("official_wordle_common") as TextAsset;
+        solutions = textFile.text.Split('\n');
+    }
+
+    private void SetRandomWord()
+    {
+        word = solutions[Random.Range(0, solutions.Length)];
+        word = word.ToLower().Trim();
+    }
+
+    private void SubmitRow(Row row)
+    {
+        string remaining = word;
+
+        for (int i = 0; i < row.tiles.Length; i++)
+        {
+            Tile tile = row.tiles[i];
+            if (tile.letter == word[i])
+            {
+                tile.SetState(correctState);
+
+                remaining = remaining.Remove(i, 1);
+                remaining = remaining.Insert(i, " ");
+            }
+            else if (!word.Contains(tile.letter))
+            {
+                tile.SetState(incorrectState);
+            }
+        }
+
+        for (int i = 0; i < row.tiles.Length; i++)
+        {
+            Tile tile = row.tiles[i];
+
+            if (tile.state != correctState && tile.state != incorrectState)
+            {
+                if (remaining.Contains(tile.letter))
+                {
+                    tile.SetState(wrongSpotState);
+
+                    int index = remaining.IndexOf(tile.letter);
+                    remaining = remaining.Remove(index, 1);
+                    remaining = remaining.Insert(index, " ");
+                }
+                else
+                {
+                    tile.SetState(incorrectState);
+                }
+            }
+        }
+
+        rowIndex++;
+        columnIndex = 0;
+
+        if (rowIndex >= rows.Length)
+        {
+            this.enabled = false;
+        }
+    }
+}
